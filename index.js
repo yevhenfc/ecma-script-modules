730eb3dfb3b356952e439278697706816a301286
yevhenfc <yevhenfc@gmail.com>
2022-09-10 22:31:02 +0300

    examples with two functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// import {pow, pow2} from "./math.js";
+// import * as m from "./math.js";
+import m from "./math.js";
+
+const y=12345
+
+const res = m.pow2(3);
+
+
+
+console.log(res);
+console.log(m.pow(3,3));
+console.log(m.pow(3,4));
+console.log(m.summa(3,4));
---
+++ math.js
@@ -0,0 +1,10 @@
+const x=100;
+
+const pow2 = (base) => base**2;
+
+const pow = (base, exp) => base**exp;
+
+const summa = (a, b) => a + b;
+
+
+export default {pow, pow2, summa};
